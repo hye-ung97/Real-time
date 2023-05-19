b19dbe6e02a673c4ba14754ceb34f5d38cfc8850
hye-ung97 <117243197+hye-ung97@users.noreply.github.com>
2023-05-19 16:54:41 +0900

    Update Alarm.py

--- Backend/Alarm.py
+++ Backend/Alarm.py
@@ -3,7 +3,7 @@ import time
 import schedule
 
 def alarm():
-    conn = pymysql.connect(host="capruby07.cafe24.com", port=3306, user="capruby07", passwd="fnql0707!", db="capruby07", charset='utf8')
+    conn = pymysql.connect(host="host.com", port=3306, user="user", passwd="password!", db="db",  charset='utf8')
     cur = conn.cursor()
 
     sql = 'select * from insertword'
